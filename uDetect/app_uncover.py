693d17eac3d9d6582118ecb7c81944c1204f8a41
tinsaeabreham45 <111001136+tinsaeabreham45@users.noreply.github.com>
2025-05-06 12:48:51 +0300

    Refactor comments for clarity and update description in Gradio interface for potato detection app

--- uDetect/app_uncover.py
+++ uDetect/app_uncover.py
@@ -8,20 +8,21 @@ import os
 """
 
 this script is for peeled potato detection using YOLOv8 model
+
 """
 
 # Load the YOLO model
 model = YOLO("uDetect/uncovered_potato.pt")
 
 def predict_potato(image):
-    # Convert Gradio image (PIL format) to OpenCV format
+    
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    # Run YOLO prediction
+    
     results = model.predict(source=image, show=False, conf=0.6)
     
-    # Draw bounding boxes and labels
+    
     for r in results:
         for box in r.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -29,22 +30,22 @@ def predict_potato(image):
             conf = float(box.conf[0])
             label = model.names[cls_id]
             
-            # Draw rectangle and label
+            
             cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(image, f"{label} ({conf:.2f})", (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
     
-    # Convert back to RGB for Gradio display
+    
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 # Create Gradio interface
 iface = gr.Interface(
     fn=predict_potato,
-    inputs=gr.Image(type="pil"),  # Allow image upload
+    inputs=gr.Image(type="pil"),  
     outputs=gr.Image(type="pil"),
     title="Potato Defect Detection with YOLO11",
-    description="Upload an image of a potato to detect if it's good or defective using a trained YOLO11 model."
+    description="Upload an image of a potato to detect if it's covered or uncovered using a trained YOLO11 model."
 )
 
 # Launch the app
